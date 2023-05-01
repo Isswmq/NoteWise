f5762f3fa4dcaaad84652baf533caaf4cefa1fed
isswmq <isswmq@gmail.com>
2023-05-01 22:54:51 +0300

    added help method

--- src/main/java/org/isswqm/notewise/NoteWise.java
+++ src/main/java/org/isswqm/notewise/NoteWise.java
@@ -1,14 +1,11 @@
 package org.isswqm.notewise;
 
 import org.isswqm.notewise.handlers.HelpHandler;
-import org.isswqm.notewise.handlers.StickerHandler;
 import org.isswqm.notewise.view.NoteWiseUI;
 import org.telegram.telegrambots.bots.DefaultAbsSender;
 
 import org.telegram.telegrambots.bots.DefaultBotOptions;
-import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
@@ -16,48 +13,32 @@ import org.telegram.telegrambots.meta.generics.LongPollingBot;
 import org.telegram.telegrambots.util.WebhookUtils;
 
 public class NoteWise extends DefaultAbsSender implements LongPollingBot {
-
-    final String stickerId = "CAACAgIAAxkBAAMwZDwgAz3UPAYlnD-XqbBOTspDkAsAAjkEAALpVQUYOLOBVDvue3kvBA";
-
-
     protected NoteWise(DefaultBotOptions options, String botToken) {
         super(options, botToken);
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-        try {
-            if(update.hasMessage() && update.getMessage().hasText() && !update.getMessage().getText().isEmpty()){
-                String chatId = String.valueOf(update.getMessage().getChatId());
-                SendMessage options = NoteWiseUI.options(chatId);
-                SendMessage mainMenu = NoteWiseUI.mainMenu(chatId);
 
+        if (update.hasMessage() && update.getMessage().hasText() && !update.getMessage().getText().isEmpty()) {
+            String chatId = update.getMessage().getChatId().toString();
+            String text = update.getMessage().getText();
+            SendMessage mainMenu = NoteWiseUI.mainMenu(chatId);
+            try {
+                execute(mainMenu);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+                throw new RuntimeException(e);
+            }
+
+            if(text.equalsIgnoreCase("help")){
+                SendMessage help = HelpHandler.help(chatId);
                 try {
-                    execute(mainMenu);
-                    execute(options);
+                    execute(help);
                 } catch (TelegramApiException e) {
-                    e.printStackTrace();
                     throw new RuntimeException(e);
                 }
-
-                if (update.hasCallbackQuery()) {
-                    CallbackQuery callbackQuery = update.getCallbackQuery();
-                    String data = callbackQuery.getData().toLowerCase();
-                    System.out.println(data);
-                    System.out.println(data.equals("help"));
-                    if (data.equals("help")) {
-                        System.out.println("data equals help");
-                        try {
-                            SendMessage help = HelpHandler.help(chatId);
-                            execute(help);
-                        } catch (TelegramApiException e) {
-                            throw new RuntimeException(e);
-                        }
-                    }
-                }
             }
-        }catch (Exception e){
-            e.printStackTrace();
         }
     }
 
--- src/main/java/org/isswqm/notewise/view/NoteWiseUI.java
+++ src/main/java/org/isswqm/notewise/view/NoteWiseUI.java
@@ -4,7 +4,6 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
 import java.util.ArrayList;
@@ -38,38 +37,38 @@ public class NoteWiseUI {
         return message;
     }
 
-    public static SendMessage mainMenu(String chatID){
-        SendMessage message = new SendMessage();
-        message.setChatId(chatID);
-        message.setText("hello");
+        public static SendMessage mainMenu(String chatID){
+            SendMessage message = new SendMessage();
+            message.setChatId(chatID);
+            message.setText("Hello! Please select an option:");
+    
+            ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
+            keyboardMarkup.setResizeKeyboard(true);
+            keyboardMarkup.setOneTimeKeyboard(true);
+            List<KeyboardRow> keyboardRows = new ArrayList<>();
+    
+            KeyboardRow row = new KeyboardRow();
 
-        ReplyKeyboardMarkup keyboardMarkup = new ReplyKeyboardMarkup();
-        keyboardMarkup.setResizeKeyboard(true);
-        List<KeyboardRow> keyboardRows = new ArrayList<>();
+            row.add("Add Note");
+            row.add("View Notes");
+            row.add("Edit Note");
+            keyboardRows.add(row);
 
-        KeyboardRow row = new KeyboardRow();
+            row = new KeyboardRow();
+            row.add("Delete Note");
+            row.add("Search Note");
+            row.add("Reminders");
+            keyboardRows.add(row);
 
-        row.add("Add Note");
-        row.add("View Notes");
-        row.add("Edit Note");
-        keyboardRows.add(row);
-        row = new KeyboardRow();
-
-        row.add("Delete Note");
-        row.add("Search Note");
-        row.add("Reminders");
-        keyboardRows.add(row);
-        row = new KeyboardRow();
-
-        row.add("Categories");
-        row.add("Settings");
-        row.add("Help");
-        keyboardRows.add(row);
-
-        keyboardMarkup.setKeyboard(keyboardRows);
-
-        message.setReplyMarkup(keyboardMarkup);
-
-        return message;
-    }
+            row = new KeyboardRow();
+            row.add("Categories");
+            row.add("Settings");
+            row.add("Help");
+            keyboardRows.add(row);
+    
+            keyboardMarkup.setKeyboard(keyboardRows);
+            message.setReplyMarkup(keyboardMarkup);
+    
+            return message;
+        }
 }
